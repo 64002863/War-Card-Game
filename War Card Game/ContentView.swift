358ac309e60665e8bd99901b05cf472e33af55ae
64002863 <64002863@ep-student.org>
2024-02-23 11:12:36 -0600

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,105 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by 64002863 on 2/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var playerCard = "card2"
+    @State var cpuCard = "card3"
+    
+    @State var playerScore = 0
+    @State var cpuScore = 1000000
+    
+    var body: some View
+    {
+        
+        ZStack {
+            
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+            
+                
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+
+                
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                      
+                        Text("rishi")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                      
+                    }
+                    Spacer()
+                    VStack {
+                     
+                        Text("danny")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                      
+                    }
+                    Spacer()
+                        
+                }
+                .foregroundColor(.white)
+                Spacer()
+              
+            }
+        
+            
+        }
+    }
+    
+    func deal() {
+        
+        let playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        let cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+       
+        if playerCardValue > cpuCardValue {
+            playerScore += 1
+        }
+        else if playerCardValue < cpuCardValue {
+            cpuScore += 1
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
